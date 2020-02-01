e3c2527226d970ea58ed5d2124bdc929bb28b63d
villatrue <scarter0313@gmail.com>
2020-01-31 23:23:21 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+// import Chat from './components/Boss/Boss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ()=>(
+    <Router>
+        <Route path="/" exact component={Home} />
+        {/* <Route path="/admin" component={Boss} /> */}
+    </Router>
+);
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Link } from 'react-router-dom'
+import './Home.css';
+
+
+const Home = ()=>{
+  
+
+    return (
+        <div >
+           Welcome to pacific Pool Service
+        </div>
+    )
+}
+
+export default Home;
